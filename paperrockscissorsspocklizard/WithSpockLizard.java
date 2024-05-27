e6bfdf3eb9fd57c055b37d5682cca6ec86bdc8b0
raisaaru <149291364+raisaaru@users.noreply.github.com>
2024-05-27 07:02:07 +0300

    Update WithSpockLizard.java

--- paperrockscissorsspocklizard/WithSpockLizard.java
+++ paperrockscissorsspocklizard/WithSpockLizard.java
@@ -8,19 +8,19 @@ public class WithSpockLizard {
     public String paperRockWithSpockLizard(String player1, String player2){
         if(player1.equalsIgnoreCase(player2)){
             return(tie);
-        } else if (player1.equalsIgnoreCase("paper") & (player2.equalsIgnoreCase("rock")||
+        } else if (player1.equalsIgnoreCase("paper") && (player2.equalsIgnoreCase("rock")||
                 player2.equalsIgnoreCase("spock"))) {
             return(player1Wins);
-        }else if (player1.equalsIgnoreCase("rock") & (player2.equalsIgnoreCase("scissors") ||
+        }else if (player1.equalsIgnoreCase("rock") && (player2.equalsIgnoreCase("scissors") ||
                 player2.equalsIgnoreCase("lizard"))) {
             return(player1Wins);
-        }else if (player1.equalsIgnoreCase("scissors") & (player2.equalsIgnoreCase("paper")||
+        }else if (player1.equalsIgnoreCase("scissors") && (player2.equalsIgnoreCase("paper")||
                 player2.equalsIgnoreCase("lizard"))) {
             return(player1Wins);
-        }else if (player1.equalsIgnoreCase("lizard") & (player2.equalsIgnoreCase("paper")||
+        }else if (player1.equalsIgnoreCase("lizard") && (player2.equalsIgnoreCase("paper")||
                 player2.equalsIgnoreCase("spock"))) {
             return(player1Wins);
-        }else if (player1.equalsIgnoreCase("spock") & (player2.equalsIgnoreCase("scissors")||
+        }else if (player1.equalsIgnoreCase("spock") && (player2.equalsIgnoreCase("scissors")||
                 player2.equalsIgnoreCase("rock"))) {
             return(player1Wins);
         } else{
